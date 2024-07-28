7b9377550d737ae097f1afb7dd67693a4532d97d
jose manuel <josemanuelwar@hotmail.es>
2024-07-27 18:39:32 -0600

    Create event the Account at application

---
+++ account.common/src/main/java/com/banking/account/common/events/FoundsWithdrawnEvent.java
@@ -0,0 +1,16 @@
+package com.banking.account.common.events;
+
+import com.banking.cqrs.core.events.BaseEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class FoundsWithdrawnEvent extends BaseEvent {
+
+    private double amount;
+}
